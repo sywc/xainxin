0f0cffdfacf6c3e8a9d95d4edcfe8865adc6c827
sywc <ginngo@163.com>
2019-01-10 13:40:18 +0800

    first commit

---
+++ src/main/java/com/ruoyi/project/xianxin/controller/ClientController.java
@@ -0,0 +1,33 @@
+package com.ruoyi.project.xianxin.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user/")
+public class ClientController {
+
+    @RequestMapping("/hello")
+    private String listviel1(){
+        return "addHtml/user_details";
+    }
+
+    @RequestMapping("/list1")
+    private String listviel(){
+        return "addHtml/list";
+    }
+
+//    /**
+//     * 客户反馈
+//     * @return
+//     */
+//    @RequestMapping("/user_feedback_list")
+//    private String clientFeedback(){
+//        return "addHtml/client/user_feedback_list";
+//    }
+
+}
